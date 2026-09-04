668a6f7734766bbdfe2f2648029c0ccc0df10573
Leonardo-de-Moura <leonardo.2025pessoal@gmail.com>
2026-09-04 17:43:13 -0300

    update class for classname in svg logo.txs arquive

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -23,7 +23,7 @@ export const Logo: React.FC<LogoProps> = ({ size = 'md', showSubtitle = true }) 
     <div  className="inline-flex items-center gap-3 group focus:outline-none" title="SGE-IFCE">
       <div className={`rounded-xl bg-[#006A38] text-white flex items-center justify-center p-2 shadow-sm `}>
       
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-calendar-days"><path d="M8 2v3"/><path d="M16 2v3"/><rect x="3" y="3" width="18" height="18" rx="2"/><path d="M3 9h18"/><path d="M8 13h.01"/><path d="M12 13h.01"/><path d="M16 13h.01"/><path d="M8 17h.01"/><path d="M12 17h.01"/><path d="M16 17h.01"/></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-calendar-days"><path d="M8 2v3"/><path d="M16 2v3"/><rect x="3" y="3" width="18" height="18" rx="2"/><path d="M3 9h18"/><path d="M8 13h.01"/><path d="M12 13h.01"/><path d="M16 13h.01"/><path d="M8 17h.01"/><path d="M12 17h.01"/><path d="M16 17h.01"/></svg>
         
       </div>
       <div className="flex flex-col text-left">
