06a3d228518cac896f3d0ed867dfd8228273b97f
Leonardo-de-Moura <leonardo.2025pessoal@gmail.com>
2026-09-07 08:46:18 -0300

    delete import figma screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
-import { FigmaScreenNavigator } from './components/common/FigmaScreenNavigator';
+
 
 // Páginas de Autenticação
 import { LandingPage } from './pages/auth/LandingPage';
